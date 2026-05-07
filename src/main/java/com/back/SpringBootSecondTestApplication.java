0c04c678139dff37d3d8aa7cd435bbb477546af8
predevho <dlagusgh7894@gmail.com>
2026-05-07 16:36:33 +0900

    초기세팅

---
+++ src/main/java/com/back/SpringBootSecondTestApplication.java
@@ -0,0 +1,13 @@
+package com.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecondTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSecondTestApplication.class, args);
+    }
+
+}
