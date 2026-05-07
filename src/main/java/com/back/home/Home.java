3c1085e977023923bf487ca85c790add02c2202a
predevho <dlagusgh7894@gmail.com>
2026-05-07 17:15:48 +0900

    feat: root url 접속시 article_list 이동

---
+++ src/main/java/com/back/home/Home.java
@@ -0,0 +1,13 @@
+package com.back.home;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Home {
+
+    @GetMapping("/")
+    public String home(){
+        return "redirect:/article_list";
+    }
+}
