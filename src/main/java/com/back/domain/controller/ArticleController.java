fe98f90071aad70ce065994bad0bb592e10af3ed
predevho <dlagusgh7894@gmail.com>
2026-05-07 17:54:13 +0900

    feat: 등록시 DB 내용 저장 및 list 리다이렉트

--- src/main/java/com/back/domain/controller/ArticleController.java
+++ src/main/java/com/back/domain/controller/ArticleController.java
@@ -1,9 +1,11 @@
 package com.back.domain.controller;
 
+import com.back.domain.entity.Article;
 import com.back.domain.service.ArticleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -20,6 +22,12 @@ public class ArticleController {
     @GetMapping("/create")
     public String articleCreate() {
         return "article_create";
+    }
+
+    @PostMapping("/create")
+    public String articleCreatePost(Article article) {
+        articleService.createArticle(article.getTitle(), article.getContent());
+        return "redirect:/article/list";
     }
 
 }
--- src/main/java/com/back/domain/service/ArticleService.java
+++ src/main/java/com/back/domain/service/ArticleService.java
@@ -1,11 +1,22 @@
 package com.back.domain.service;
 
+import com.back.domain.entity.Article;
 import com.back.domain.repository.ArticleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
 public class ArticleService {
     private final ArticleRepository articleRepository;
+
+    public void createArticle(String title, String content){
+        Article article = new Article();
+        article.setTitle(title);
+        article.setContent(content);
+        article.setCreateDate(LocalDateTime.now());
+        articleRepository.save(article);
+    }
 }
