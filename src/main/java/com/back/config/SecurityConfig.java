aaee938bed17541ccda7e37d76dfcd0602ad1d09
predevho <dlagusgh7894@gmail.com>
2026-05-09 00:34:22 +0900

    fix: 로그인 성공 후 savedRequest 무시하고 리스트로 이동 및 navbar 버튼 순서 수정

--- src/main/java/com/back/config/SecurityConfig.java
+++ src/main/java/com/back/config/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
             .formLogin(form -> form
                 .loginPage("/user/login")
                 .loginProcessingUrl("/user/login")
-                .defaultSuccessUrl("/article/list")
+                .defaultSuccessUrl("/article/list", true)
             )
             .logout(logout -> logout
                 .logoutUrl("/user/logout")
