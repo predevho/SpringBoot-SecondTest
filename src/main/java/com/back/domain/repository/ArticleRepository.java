379353ca56d66133d47798bfa2dd2ff41409d308
predevho <dlagusgh7894@gmail.com>
2026-05-11 22:34:49 +0900

    feat: 검색 기능 구현

--- src/main/java/com/back/domain/repository/ArticleRepository.java
+++ src/main/java/com/back/domain/repository/ArticleRepository.java
@@ -2,8 +2,9 @@ package com.back.domain.repository;
 
 import com.back.domain.entity.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository extends JpaRepository<Article, Long>, JpaSpecificationExecutor<Article> {
 }
--- src/main/java/com/back/domain/service/ArticleService.java
+++ src/main/java/com/back/domain/service/ArticleService.java
@@ -4,7 +4,10 @@ import com.back.DataNotFoundException;
 import com.back.domain.entity.Article;
 import com.back.domain.repository.ArticleRepository;
 import com.back.user.entity.SiteUser;
+import jakarta.persistence.criteria.*;
 import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.Nullable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -15,6 +18,22 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class ArticleService {
     private final ArticleRepository articleRepository;
+
+    private Specification<Article> search(String kw){
+        return new Specification<Article>() {
+            @Nullable
+            @Override
+            public Predicate toPredicate(Root<Article> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
+                query.distinct(true);
+                return criteriaBuilder.or(
+                        criteriaBuilder.like(root.get("title"), "%" + kw + "%"),
+                        criteriaBuilder.like(root.get("content"), "%" + kw + "%"),
+                        criteriaBuilder.like(root.get("author").get("username"), "%" + kw + "%"),
+                        criteriaBuilder.like(root.get("author").get("nickname"), "%" + kw + "%")
+                );
+            }
+        };
+    }
 
     public void createArticle(String title, String content, SiteUser author) {
         Article article = new Article();
@@ -37,8 +56,9 @@ public class ArticleService {
         articleRepository.delete(article);
     }
 
-    public List<Article> getList() {
-        return articleRepository.findAll();
+    public List<Article> getList(String kw) {
+        Specification<Article> search = search(kw);
+        return articleRepository.findAll(search);
     }
 
     public Article getArticleById(Long id) {
